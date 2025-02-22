3839323ac28fbd0989addf701857c9c8bdbcdb15
Moses <docgang12@gmail.com>
2025-02-22 17:57:28 -0600

    Learned abour DOM

---
+++ DOM/script.js
@@ -0,0 +1,27 @@
+const body = document.body;
+
+// const div = document.createElement("div");
+
+// body.append(div);
+
+// const div2 = document.createElement("div");
+
+// div.append(div2);
+
+// div2.remove();
+
+// div.setAttribute("title", "Box");
+
+// div.id = "Wawgan";
+
+// console.log(div.id);
+// console.log(div.title);
+
+const div = document.querySelector("div");
+
+div.classList.toggle("h2");
+
+div.innerText = "Hello ";
+
+div.style.backgroundColor = "#6EEB83";
+div.style.color = "#DDA15E";
